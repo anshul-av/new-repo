86d02331dff2c7c880aa9e9f3519ce3118e41169
root <root@ip-172-31-16-71.us-east-2.compute.internal>
2021-02-09 08:49:11 +0000

    making new repo

---
+++ src/main/java/new-repo/App.java
@@ -0,0 +1,13 @@
+package new-repo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
